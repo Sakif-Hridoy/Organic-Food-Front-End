1577e7a7adb9620c816d4889a44bd45ba0ac0c9d
Sakif <sakifworkspace@gmail.com>
2022-03-29 14:30:51 +0600

    add react fontawesome social icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap-4-react/dist/bootstrap-4-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function App() {
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Orders from './components/Orders/Orders';
+import Admin from './components/Admin/Admin';
+import Deals from './components/Deals/Deals';
+import Login from './components/Login/Login';
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/orders">Orders</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+            <li>
+              <Link to="/deals">Deals</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        <Route path="/login">
+            <Login />
+          </Route>
+
+        <Route path="/deals">
+            <Deals />
+          </Route>
+
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/orders">
+            <Orders />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
