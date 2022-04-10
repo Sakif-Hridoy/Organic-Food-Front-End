40c2dab7bb3c7f4d9cb3bc2bb9d8e941638dc54a
sakif <sakifworkspace@gmail.com>
2022-04-10 10:50:17 +0600

    add table header to show dynamic user data

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,34 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../App';
 
 const Orders = () => {
     const [loggedInUser,setLoggedInUser] = useContext(UserContext);
     const [orders,setOrders] = useState([]);
     const email = loggedInUser.email;
-    console.log(loggedInUser,orders,loggedInUser.email)
+    console.log(loggedInUser,orders,loggedInUser.email);
+
+    useEffect(()=>{
+        fetch(`http://localhost:4545/orders?${email}`,{
+
+        }).then(res=>res.json())
+        .then(data=>setOrders(data))
+    })
     return (
         <div>
-            <h1>This is Orders</h1>
+            <h2>Your Orders</h2>
+            <h3>User Name: {loggedInUser.name}</h3>
+
+            <table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Product Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Date</th>
+    </tr>
+  </thead>
+</table>
+
+
         </div>
     );
 };
