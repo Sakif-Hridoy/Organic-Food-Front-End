383cd81b74185c493760e3cebbccc457782f5ea0
sakif <sakifworkspace@gmail.com>
2022-04-10 10:31:59 +0600

    show ordered product into the users ordered product console

--- src/App.js
+++ src/App.js
@@ -50,7 +50,8 @@ export default function App() {
 
         <PrivateRoute path="/admin">
         <Admin />
-          </PrivateRoute>    
+          </PrivateRoute>  
+            
         <PrivateRoute path="/orders">
         <Orders />
           </PrivateRoute> 
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../../App';
 
 const Orders = () => {
+    const [loggedInUser,setLoggedInUser] = useContext(UserContext);
+    const [orders,setOrders] = useState([]);
+    const email = loggedInUser.email;
+    console.log(loggedInUser,orders,loggedInUser.email)
     return (
         <div>
             <h1>This is Orders</h1>
